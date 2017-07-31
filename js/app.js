ff202bcd1770c5fe2e8e780906728d77f52f30da
vcordero07 <vcordero07@gmail.com>
2017-07-30 20:19:21 -0400

    wip-refactoring

--- js/app.js
+++ js/app.js
@@ -1,43 +1,73 @@
-// let hideShow = (toHide = [], toShow = []) => {
-//   toHide.forEach(function(item, indx) {
-//     $(item).hide()
-//   });
-//   toShow.forEach(function(item, indx) {
-//     $(item).show()
-//   });
-// };
+//LABELS
+//JG JOB GUARANTEE
+//JA JOB ASSISTANCE
+//OP ONLINE PROGRAM
+//IP IMMERSIVE PROGRAM
+let careerTrack = 'ONLINE / IMMERSIVE';
+let jobGuarantee = 'JOB GUARANTEE';
+let jobAssistance = 'JOB ASSISTANCE';
 
 let createEventListeners = () => {
   $('[data-toggle="tooltip"]').tooltip();
 
+  console.log($('#online-immersive-opt').text());
+  console.log($('#job-guarantee-opt').text());
+  console.log($('#job-assistance-opt').text());
   $('.online-dropdown li > a').on('click', (event) => {
     console.log('event:', event);
-    console.log('event.currentTarget:', event.currentTarget.innerHTML);
+    console.log('event.currentTarget:', event.currentTarget.getAttribute('data-opt-value'));
+    careerTrack = event.currentTarget.getAttribute('data-opt-value');
     $('#online-immersive-opt').text(event.currentTarget.innerHTML);
-    if ($('#online-immersive-opt').text() === 'ONLINE') {
-      $('img').addClass('filterImg');
-      $('img.onlineProg').removeClass('filterImg');
-    } else if (($('#online-immersive-opt').text() === 'IMMERSIVE')) {
-      $('img').addClass('filterImg');
-      $('img.immersiveProg').removeClass('filterImg');
-    }
 
   });
 
-  $('.guarantees-dropdown li > a').on('click', (event) => {
-    console.log('event:', event);
-    console.log('event.currentTarget:', event.currentTarget.innerHTML);
-    $('#guarantees-job-opt').text(event.currentTarget.innerHTML);
+  $('.guarantee-dropdown li > a').on('click', (event) => {
+    console.log('event:', $('#job-guarantee-opt'));
+    console.log('event.currentTarget:', event.currentTarget.getAttribute('data-opt-value'));
+    jobGuarantee = event.currentTarget.getAttribute('data-opt-value');
+    $('#job-guarantee-opt').text(event.currentTarget.innerHTML);
+
   });
 
   $('.assistance-dropdown li > a').on('click', (event) => {
     console.log('event:', event);
-    console.log('event.currentTarget:', event.currentTarget.innerHTML);
+    console.log('event.currentTarget:', event.currentTarget.getAttribute('data-opt-value'));
+    jobAssistance = event.currentTarget.getAttribute('data-opt-value');
     $('#job-assistance-opt').text(event.currentTarget.innerHTML);
   });
 
   $('.search-btn').on('click', (event) => {
-    $('img').removeClass('filterImg');
+
+    let careerTrackSelector = '';
+    if (careerTrack !== 'ONLINE / IMMERSIVE') {
+      careerTrackSelector = `.${careerTrack}`;
+    }
+    console.log('careerTrack, careerTrackSelector:', careerTrack, careerTrackSelector);
+
+
+    let jobGuaranteeSelector = '';
+    if (jobGuarantee !== 'JOB GUARANTEE') {
+      jobGuaranteeSelector = `.${jobGuarantee}`;
+    }
+    console.log('jobGuarantee, jobGuaranteeSelector:', jobGuarantee, jobGuaranteeSelector);
+
+
+    let jobAssistanceSelector = '';
+    if (jobAssistance !== 'JOB ASSISTANCE') {
+      jobAssistanceSelector = `.${jobAssistance}`;
+    }
+    console.log('jobAssistance, jobGuaranteeSelector:', jobAssistance, jobGuaranteeSelector);
+
+    let combinedSelector = careerTrackSelector + jobGuaranteeSelector + jobAssistanceSelector;
+    console.log('combinedSelector:', combinedSelector);
+
+    if (combinedSelector === "") {
+      $('.logo-container img').addClass('active');
+    } else {
+      $('.logo-container img').removeClass('active');
+      $(combinedSelector).addClass('active');
+    }
+
   });
 
 };
